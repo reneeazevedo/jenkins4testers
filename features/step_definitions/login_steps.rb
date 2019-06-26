54e5c8b5b8d3ee39cf7502546172c04a0dc6274a
reneeazevedo <renee.moura@hotmail.com>
2019-06-25 23:17:24 -0300

    teste alterando cucumber.yaml

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -7,7 +7,7 @@ Quando("eu faço login com {string} e {string}") do |email, password|
 end
 
 Então("devo ser autenticado") do
-  expect(get_token.length).to be 147
+  expect(@get_token.length).to be 147
 end
 
 Então("devo ver {string} na área logada") do |expect_name|
@@ -15,7 +15,7 @@ Então("devo ver {string} na área logada") do |expect_name|
 end
 
 Então("não devo ser autenticado") do
-  expect(get_token).to be nil
+  expect(@get_token).to be nil
 end
 
 Então("devo ver a mensagem de alerta {string}") do |expect_message|
--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -3,10 +3,11 @@
 module Helpers
   def get_token
     timeout = Capybara.default_max_wait_time
-    2.times do
+    10.times do
       js_script = 'return window.localStorage.getItem("default_auth_token");'
       @token = page.execute_script(js_script)
       break if @token != nil
+      puts @token
       sleep 1
     end
     @token
