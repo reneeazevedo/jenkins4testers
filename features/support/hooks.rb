c465a25c4171aacd207a7f1c6ba704e1d3a669ab
reneeazevedo <renee.moura@hotmail.com>
2019-06-26 21:17:23 -0300

    anexando screenshots nos relatórios

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -12,3 +12,8 @@ Before("@login") do
   @login_page.with(user["email"], user["pass"])
 end
 
+After do |scenario|
+  screenshot = page.save_screenshot("log/screenshots/#{scenario__id__}.png")
+  embed(screenshot, "image/png", "Screenshot")
+end
+
